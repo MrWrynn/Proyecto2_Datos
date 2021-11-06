2b981d822446d551dc94b291745119858a3d2f5e
MrWrynn <64748923+MrWrynn@users.noreply.github.com>
2021-11-06 07:52:35 -0600

    Update ServidorHilo.java

--- ServidorHilo.java
+++ ServidorHilo.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ejercicio_sockets_ddr_8;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
